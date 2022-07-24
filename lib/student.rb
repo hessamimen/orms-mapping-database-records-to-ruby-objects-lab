1b532ecc939b1429a4e3f9ba1d6cb89f1201ed43
Hessam Imen <hessam.imen@gmail.com>
2022-07-24 16:26:58 +1000

    commit tree 65d2befe6024ee2938c232c4b7f7bf87d523b898

--- lib/student.rb
+++ lib/student.rb
@@ -32,6 +32,7 @@ class Student
 #    creates an instance with corresponding attribute values
   def self.new_from_db(row)
     # create a new Student object given a row from the database
+
     new_student = self.new 
     new_student.id = row[0]
     new_student.name =  row[1]
